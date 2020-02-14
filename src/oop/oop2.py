6e17f546b7eb1d206c342a4cd1f7cb9ee6450e2e
masonkarsevar <masonkarsevar@gmail.com>
2020-02-14 10:04:10 -0800

    changed some logic in oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -26,9 +26,8 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        super().__init__(num_wheels)
-        self.num_wheels = num_wheels 
+    def __init__(self):
+        super().__init__(num_wheels=2)
 
     def drive(self):
         return 'BRAAAP!!'
